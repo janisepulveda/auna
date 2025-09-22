40692ce7d7e5fe67de3d1c11b4315acde6f46385
janisepulveda <janis.sepulveda@mail.udp.cl>
2025-09-22 18:32:44 -0300

    conexión BLE

--- auna_septiembre_bt/macos/Flutter/GeneratedPluginRegistrant.swift
+++ auna_septiembre_bt/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,9 +6,11 @@ import FlutterMacOS
 import Foundation
 
 import reactive_ble_mobile
+import shared_preferences_foundation
 import universal_ble
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ReactiveBlePlugin.register(with: registry.registrar(forPlugin: "ReactiveBlePlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   UniversalBlePlugin.register(with: registry.registrar(forPlugin: "UniversalBlePlugin"))
 }
